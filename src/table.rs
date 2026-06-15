e9afbb32ff96a8a7e1b7fc823934ab19fbd00423
larakramsobin82 <edqbr56457927@hotmail.com>
2026-06-14 22:36:08 -0400

    [bug] lint/clippy on src/table.rs

--- src/table.rs
+++ src/table.rs
@@ -252,6 +252,7 @@ impl Table {
                 }
 
                 let allocated_idx = self.push_page::<T>(ingredient, memo_types.clone());
+                return;
                 assert_eq!(
                     allocated_idx, page_idx,
                     "allocated index does not match requested index"
